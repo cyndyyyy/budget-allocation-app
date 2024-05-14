8f2ba5570d9793f4b07b9c2f87ba12ca0e69e652
Ahmad Jamal EL-Youssef <115467278+aj-elyoussef@users.noreply.github.com>
2024-05-14 18:24:57 +0000

    Update (still in progress)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -23,11 +23,6 @@ const ExpenseItem = (props) => {
             payload: expense
         });
 
-        dispatch({
-            type: 'MINUS_EXPENSE',
-            payload: expense
-        });
-
     }
 
     return (
@@ -35,7 +30,6 @@ const ExpenseItem = (props) => {
         <td>{props.name}</td>
         <td>${props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
